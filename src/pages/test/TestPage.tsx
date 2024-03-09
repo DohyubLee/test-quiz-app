928866b3f5db024143c4f452285a74f9ad55b9ef
DohyubLee <hani7304@gmail.com>
2024-03-09 19:58:57 +0900

    스켈레톤 적용

--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -1,6 +1,14 @@
 import useStore, { CustomQuiz } from "@stores/store";
 import { formatTime } from "@utils/formatUtile";
-import { Button, Card, Radio, RadioChangeEvent, Space, Typography } from "antd";
+import {
+  Button,
+  Card,
+  Radio,
+  RadioChangeEvent,
+  Skeleton,
+  Space,
+  Typography,
+} from "antd";
 import React, { useCallback, useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
@@ -57,71 +65,77 @@ function TestPage() {
         paddingTop: 40,
       }}
     >
-      {quizList
-        .filter(
-          (item: CustomQuiz, index: number) => item.order === Number(testId)
-        )
-        .map((item) => {
-          const decodedText = parser.parseFromString(item.question, "text/html")
-            .body.textContent;
-          const options = [item.correct_answer, ...item.incorrect_answers];
+      {quizList.length > 0 ? (
+        quizList
+          .filter(
+            (item: CustomQuiz, index: number) => item.order === Number(testId)
+          )
+          .map((item) => {
+            const decodedText = parser.parseFromString(
+              item.question,
+              "text/html"
+            ).body.textContent;
+            const options = [item.correct_answer, ...item.incorrect_answers];
 
-          return (
-            <Card
-              rootClassName="quiz-card"
-              title={decodedText}
-              extra={
-                <Button
-                  type="primary"
-                  size="small"
-                  onClick={() => {
-                    nextQuiz(Number(testId));
-                  }}
-                  disabled={!item.selectedAnswer}
-                >
-                  다음 문제
-                </Button>
-              }
-              loading={loading}
-            >
-              <Radio.Group
-                onChange={(e) => onChange(e, item.order)}
-                value={item.selectedAnswer}
+            return (
+              <Card
+                rootClassName="quiz-card"
+                title={decodedText}
+                extra={
+                  <Button
+                    type="primary"
+                    size="small"
+                    onClick={() => {
+                      nextQuiz(Number(testId));
+                    }}
+                    disabled={!item.selectedAnswer}
+                  >
+                    다음 문제
+                  </Button>
+                }
+                loading={loading}
               >
-                <Space direction="vertical">
-                  {options.map((option) => {
-                    return (
-                      <Radio value={option}>
-                        <div>
-                          {item.selectedAnswer ? (
-                            option === item.selectedAnswer ? (
-                              <Text
-                                type={
-                                  option === item.correct_answer
-                                    ? "success"
-                                    : "danger"
-                                }
-                              >
-                                {option}{" "}
-                                {option === item.correct_answer
-                                  ? "정답입니다."
-                                  : "오답입니다."}
-                              </Text>
+                <Radio.Group
+                  onChange={(e) => onChange(e, item.order)}
+                  value={item.selectedAnswer}
+                >
+                  <Space direction="vertical">
+                    {options.map((option) => {
+                      return (
+                        <Radio value={option}>
+                          <div>
+                            {item.selectedAnswer ? (
+                              option === item.selectedAnswer ? (
+                                <Text
+                                  type={
+                                    option === item.correct_answer
+                                      ? "success"
+                                      : "danger"
+                                  }
+                                >
+                                  {option}{" "}
+                                  {option === item.correct_answer
+                                    ? "정답입니다."
+                                    : "오답입니다."}
+                                </Text>
+                              ) : (
+                                <Text>{option}</Text>
+                              )
                             ) : (
                               <Text>{option}</Text>
-                            )
-                          ) : (
-                            <Text>{option}</Text>
-                          )}
-                        </div>
-                      </Radio>
-                    );
-                  })}
-                </Space>
-              </Radio.Group>
-            </Card>
-          );
-        })}
+                            )}
+                          </div>
+                        </Radio>
+                      );
+                    })}
+                  </Space>
+                </Radio.Group>
+              </Card>
+            );
+          })
+      ) : (
+        <Skeleton />
+      )}
       {formatTime(currentTime)}
     </Space>
   );
