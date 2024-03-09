70ebca6a2fb950e2631d1eedfc6fd587e9534796
DohyubLee <hani7304@gmail.com>
2024-03-09 19:20:14 +0900

    결과 페이지 UI

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,6 +1,6 @@
 import useStore from "@stores/store";
 import { Button, Space } from "antd";
-import React from "react";
+import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function IndexPage() {
@@ -12,6 +12,9 @@ function IndexPage() {
     setIntervalId(intervalId);
     navigate("/test/1");
   };
+  useEffect(() => {
+    // 최초에서 모든 데이터 리셋처리 여기서 해야함
+  }, []);
   return (
     <div
       style={{
--- src/pages/result/ResultPage.tsx
+++ src/pages/result/ResultPage.tsx
@@ -1,7 +1,82 @@
+import useStore from "@stores/store";
+import { formatTime } from "@utils/formatUtile";
+import {
+  Badge,
+  Button,
+  Card,
+  Col,
+  Flex,
+  Progress,
+  Row,
+  Space,
+  Typography,
+} from "antd";
 import React from "react";
+const { Title, Text } = Typography;
 
 function ResultPage() {
-  return <div>intervalId</div>;
+  const { currentTime, quizList } = useStore();
+  return (
+    <div>
+      <Title>결과 확인하기</Title>
+      <Title level={3}>소요시간 : {formatTime(currentTime)}</Title>
+      <Card
+        title="정답률"
+        extra={
+          <Button
+            type="primary"
+            size="small"
+            onClick={() => {
+              console.log("오답노트 보러가기");
+            }}
+          >
+            오답노트
+          </Button>
+        }
+      >
+        <Row justify={"center"} gutter={80}>
+          <Col
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-around",
+            }}
+          >
+            <Badge
+              status="success"
+              text={
+                <p style={{ display: "inline" }}>
+                  정답 개수 <b>7</b>
+                </p>
+              }
+            />
+            <Badge
+              status="error"
+              text={
+                <p style={{ display: "inline" }}>
+                  오답 개수 <b>7</b>
+                </p>
+              }
+            />
+          </Col>
+          <Col>
+            {/* <Text strong>정답률</Text> */}
+            <Progress
+              percent={30}
+              success={{
+                percent: 30,
+              }}
+              type="circle"
+              trailColor="#FF4D4F"
+            />
+          </Col>
+        </Row>
+      </Card>
+      <Flex justify="center" style={{ paddingTop: 40 }}>
+        <Button>처음으로 돌아가기</Button>
+      </Flex>
+    </div>
+  );
 }
 
 export default ResultPage;
