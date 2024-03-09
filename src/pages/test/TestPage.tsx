f7dea9ba98b9baed33a2bbadb5c8ad925884dd0d
DohyubLee <hani7304@gmail.com>
2024-03-09 10:36:53 +0900

    스토어 생성

--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -1,7 +1,23 @@
+import useStore from "@stores/store";
+import { Button } from "antd";
 import React from "react";
 
 function TestPage() {
-  return <div>TestPage</div>;
+  const { count, inc } = useStore();
+  console.log("couhnt", count);
+  return (
+    <div>
+      TestPage
+      {count}
+      <Button
+        onClick={() => {
+          inc();
+        }}
+      >
+        +++
+      </Button>
+    </div>
+  );
 }
 
 export default TestPage;
