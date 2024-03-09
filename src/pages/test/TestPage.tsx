5ac6b5a8b8ddf397aef5caa4e3ee766b2167c81c
DohyubLee <hani7304@gmail.com>
2024-03-09 16:52:37 +0900

    문제 데이터 렌더링

--- src/pages/test/TestPage.tsx
+++ src/pages/test/TestPage.tsx
@@ -1,34 +1,121 @@
-import useStore from "@stores/store";
-import { Button, Card, Space } from "antd";
-import React, { useEffect, useState } from "react";
+import useStore, { CustomQuiz } from "@stores/store";
+import { Button, Card, Radio, RadioChangeEvent, Space, Typography } from "antd";
+import React, { useCallback, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const { Text } = Typography;
+const parser = new DOMParser();
 
 function TestPage() {
-  const { fetchQuizList, quizList } = useStore();
-  const [currentQuizNum, setCurrentQuizNum] = useState(0);
+  const { fetchQuizList, quizList, setQuizList } = useStore();
+  let { testId } = useParams();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
   console.log("quizList", quizList);
+  console.log("testId", testId);
+
+  const fetchList = useCallback(async () => {
+    setLoading(true);
+    await fetchQuizList(10);
+    setLoading(false);
+  }, []);
+
+  const nextQuiz = useCallback(
+    (testId: number) => {
+      if (testId < quizList.length) {
+        navigate(`/test/${testId + 1}`);
+      }
+    },
+    [quizList.length]
+  );
+
+  const onChange = (e: RadioChangeEvent, order: number) => {
+    const answer = e.target.value;
+    setQuizList(order, answer);
+  };
 
   useEffect(() => {
-    // fetchQuizList(10);
+    // fetchList();
   }, []);
   return (
     <Space
       direction="vertical"
-      size="middle"
+      // size="middle"
       style={{
         display: "flex",
+        paddingTop: 40,
       }}
     >
-      <Card
-        title="Default size card"
-        // extra={<a href="#">More</a>}
-        // style={{ width: 300 }}
-      >
-        <p>Card content</p>
-        <p>Card content</p>
-        <p>Card content</p>
-      </Card>
+      {quizList
+        .filter(
+          (item: CustomQuiz, index: number) => item.order === Number(testId)
+        )
+        .map((item) => {
+          const decodedText = parser.parseFromString(item.question, "text/html")
+            .body.textContent;
+          console.log("item", item);
+          const options = [item.correct_answer, ...item.incorrect_answers];
+          console.log("options", options);
+          return (
+            <Card
+              rootClassName="quiz-card"
+              title={decodedText}
+              extra={
+                <Button
+                  type="primary"
+                  size="small"
+                  onClick={() => {
+                    nextQuiz(Number(testId));
+                  }}
+                  disabled={!item.selectedAnswer}
+                >
+                  다음 문제
+                </Button>
+              }
+              loading={loading}
+            >
+              <Radio.Group
+                onChange={(e) => onChange(e, item.order)}
+                value={item.selectedAnswer}
+              >
+                <Space direction="vertical">
+                  {options.map((option) => {
+                    return (
+                      <Radio value={option}>
+                        <div>
+                          {item.selectedAnswer ? (
+                            option === item.selectedAnswer ? (
+                              <Text
+                                type={
+                                  option === item.correct_answer
+                                    ? "success"
+                                    : "danger"
+                                }
+                              >
+                                {option}{" "}
+                                {option === item.correct_answer
+                                  ? "정답입니다."
+                                  : "오답입니다."}
+                              </Text>
+                            ) : (
+                              <Text>{option}</Text>
+                            )
+                          ) : (
+                            <Text>{option}</Text>
+                          )}
+                        </div>
+                      </Radio>
+                    );
+                  })}
+                </Space>
+              </Radio.Group>
+            </Card>
+          );
+        })}
     </Space>
   );
 }
-
+{
+  /* <Text type="success">{option}</Text> */
+}
 export default TestPage;
--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,17 +1,27 @@
 import { fetchQuizList } from "@apis/quizApi";
 import { create } from "zustand";
+import _, { cloneDeep } from "lodash";
 
-interface Quiz {
+export interface Quiz {
   category: string;
   correct_answer: string;
   difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+}
+
+export interface CustomQuiz extends Quiz {
+  order: number;
+  selectedAnswer?: string;
 }
 
 interface Store {
   count: number;
   inc: () => void;
-  fetchQuizList: (amount?: number) => void;
-  quizList: any[];
+  fetchQuizList: (amount?: number) => Promise<any>;
+  quizList: CustomQuiz[];
+  setQuizList: (order: number, answer: string) => void;
 }
 
 const useStore = create<Store>((set) => ({
@@ -22,7 +32,25 @@ const useStore = create<Store>((set) => ({
   quizList: [],
   fetchQuizList: async (amount = 10) => {
     const data = await fetchQuizList(amount);
-    set((state) => ({ quizList: data.results }));
+    set((state) => ({
+      quizList: data.results.map((item: Quiz, index: number) => ({
+        ...item,
+        order: index + 1,
+      })),
+    }));
+  },
+  setQuizList: (order: number, answer: string) => {
+    set((state) => {
+      const copy = cloneDeep(state.quizList);
+      return {
+        quizList: copy.map((item) => {
+          if (item.order === order) {
+            return { ...item, selectedAnswer: answer };
+          }
+          return item;
+        }),
+      };
+    });
   },
 }));
 
